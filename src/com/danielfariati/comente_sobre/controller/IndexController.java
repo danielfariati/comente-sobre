d9ff66cc05ffc150f2df469c530ed25a5f292d5b
Daniel Faria <daniel.gomes@concretesolutions.com.br>
2012-07-03 18:57:30 -0300

    retirando include desnecessario

---
+++ src/com/danielfariati/comente_sobre/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.danielfariati.comente_sobre.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class IndexController {
+
+	@Path("/")
+	public void index() {
+	}
+
+}
