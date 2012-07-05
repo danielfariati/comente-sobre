7eaacb3d6e9274dc45c97752998052c650247eaa
Daniel Faria <daniel.gomes@concretesolutions.com.br>
2012-07-05 18:44:28 -0300

    incluindo teste para metodo list

--- test/com/danielfariati/comente_sobre/controller/CommentControllerTest.java
+++ test/com/danielfariati/comente_sobre/controller/CommentControllerTest.java
@@ -2,6 +2,9 @@ package com.danielfariati.comente_sobre.controller;
 
 import static org.mockito.Mockito.verify;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -13,6 +16,7 @@ import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.util.test.MockResult;
 
 import com.danielfariati.comente_sobre.model.Comment;
+import com.danielfariati.comente_sobre.model.Subject;
 import com.danielfariati.comente_sobre.repository.CommentRepository;
 
 public class CommentControllerTest {
@@ -45,6 +49,37 @@ public class CommentControllerTest {
 		controller.save(comment);
 
 		verify(repository).save(comment);
+	}
+
+	@Test
+	public void shouldCallMethodListBySubject() {
+		Subject subject = Mockito.mock(Subject.class);
+
+		controller.list(subject);
+
+		verify(repository).loadBySubject(subject);
+	}
+
+	@Test
+	public void shouldIncludeSubjectOnResultWhenListingComments() {
+		Subject subject = Mockito.mock(Subject.class);
+
+		controller.list(subject);
+
+		verify(result).include("subject", subject);
+	}
+
+	@Test
+	public void shouldIncludeCommentListOnResultWhenListingComments() {
+		Subject subject = Mockito.mock(Subject.class);
+
+		Collection<Comment> commentList = new ArrayList<Comment>();
+
+		Mockito.when(repository.loadBySubject(subject)).thenReturn(commentList);
+
+		controller.list(subject);
+
+		verify(result).include("commentList", commentList);
 	}
 
 	@Test
