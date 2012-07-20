29858f9975f50eb0444eaf858a35f75ae72bfd7d
Daniel Faria <daniel.gomes@concretesolutions.com.br>
2012-07-20 17:18:41 -0300

    adicionando listagem de topicos na index

--- src/com/danielfariati/comente_sobre/controller/IndexController.java
+++ src/com/danielfariati/comente_sobre/controller/IndexController.java
@@ -1,13 +1,30 @@
 package com.danielfariati.comente_sobre.controller;
 
+import java.util.Collection;
+
+import com.danielfariati.comente_sobre.model.Topic;
+import com.danielfariati.comente_sobre.repository.TopicRepository;
+
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 
 @Resource
 public class IndexController {
 
+	private Result result;
+	private TopicRepository repository;
+
+	public IndexController(Result result, TopicRepository repository) {
+		this.result = result;
+		this.repository = repository;
+	}
+
 	@Path("/")
 	public void index() {
+		Collection<Topic> topicList = repository.loadAll();
+
+		result.include("topicList", topicList);
 	}
 
 }
